096c7ef96888261fe86d491e0db7c9ed9245bdc7
Dmitry Vibe <riim@yandex.ru>
2019-11-23 03:22:06 +0300

    use commonjs

--- dist/uid.js
+++ dist/uid.js
@@ -1,10 +1,13 @@
-import { randomBytes } from 'crypto';
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const crypto_1 = require("crypto");
 const UID_CHARS = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-export function uid(length = 32, chars = UID_CHARS) {
-    let bytes = randomBytes(length);
+function uid(length = 32, chars = UID_CHARS) {
+    let bytes = crypto_1.randomBytes(length);
     let r = [];
     for (let byte of bytes) {
         r.push(chars[byte % chars.length]);
     }
     return r.join('');
 }
+exports.uid = uid;
